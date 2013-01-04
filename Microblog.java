63fbd52a50d2bb4827a349507a53c5340916ea8e
Eitan Romanoff <ear7631@gmail.com>
2013-01-03 19:37:38 -0500

    Message structure implemented

--- Microblog.java
+++ Microblog.java
@@ -1,6 +1,10 @@
 import edu.rit.ds.registry.RegistryProxy;
 import edu.rit.ds.registry.AlreadyBoundException;
 import java.rmi.server.UnicastRemoteObject;
+import java.rmi.NoSuchObjectException;
+import java.rmi.RemoteException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 
 public class Microblog implements BlogRef {
@@ -37,14 +41,14 @@ public class Microblog implements BlogRef {
             ); 
         }
 
-        // Get a proxy for the Registry Server
-        registry = new RegistryProxy(registry_host, registry_port);
-
-        // Export this MicroBlog
-        UnicastRemoteObject.exportObject(this, 0);
-
-        // Bind this MicroBlog into the Registry Server
         try {
+            // Get a proxy for the Registry Server
+            registry = new RegistryProxy(registry_host, registry_port);
+
+            // Export this MicroBlog
+            UnicastRemoteObject.exportObject(this, 0);
+
+            // Bind this MicroBlog into the Registry Server
             registry.bind (this.name, this);
         }
         catch (AlreadyBoundException e) {
@@ -60,6 +64,43 @@ public class Microblog implements BlogRef {
             // TODO: Handle other remote exceptions on startup
         }
 
+    }
+
+
+    private class Message implements Comparable<Message> {
+        private int id;
+        private long time;
+        private String author;
+        private String content;
+
+        public Message(int id, long time, String author, String content) {
+            this.id = id;
+            this.time = time;
+            this.author = author;
+            this.content = content;
+        }
+
+        public int compareTo(Message other) {
+            if (this.time < other.time) { return -1; }
+            else if (this.time > other.time) { return 1; }
+            else if (this.author.compareTo(other.author) != 0) {
+                return this.author.compareTo(other.author); 
+            }
+            else if (this.id < other.id) { return -1; }
+            else if (this.id > other.id) { return 1; }
+            else { return 1; }
+        }
+
+        public String toString() {
+            Date date = new Date(time);
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd H:mm:ss");
+            String timestamp = formatter.format(date);
+            String ret;
+            ret = "--------------------------------------------------------------------------------";
+            ret = ret + "\n" + author + " -- " + "Message " + id + " -- " + timestamp;
+            ret = ret + "\n" + content;
+            return ret;
+        }
     }
 
 }
